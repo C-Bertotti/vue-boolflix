2729931a15a40cdb51adf055699cb8359b33dcde
Caterina Bertotti <bertotti4997@gmail.com>
2021-04-14 14:59:29 +0200

    Aggiunto popolari

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ var app = new Vue (
     {
         el: '#root',
         data: {
+            baseURL: 'https://api.themoviedb.org/3/',
+            api_key: '63f8ca53972c84a81c6dff2fbb6513fb',
             isActive: null,
-            baseURL: 'https://api.themoviedb.org/3/search/',
             searchedFilm: '',
+            populars: [],
             films: [],
             series: [],
             selectLanguage: 0,
@@ -16,12 +18,24 @@ var app = new Vue (
             baseURLimage: 'https://image.tmdb.org/t/p/w',
             setWidth: 300 
         },
+        mounted: function() {
+            axios.get( this.baseURL + 'trending/all/day', {
+                params: {
+                    api_key: this.api_key,
+                    language: this.languages[this.selectLanguage]
+                }
+            })
+            .then( (returnedItems) => {
+                this.populars = returnedItems.data.results;
+                console.log(this.populars);
+            });
+        },
         methods: {
             activeSearchBar: function() {
                 this.isActive == true ? this.isActive = false : this.isActive = true;
             },
             searchFilm: function() {
-                axios.get( this.baseURL + 'movie', {
+                axios.get( this.baseURL + 'search/movie', {
                     params: {
                         api_key: '63f8ca53972c84a81c6dff2fbb6513fb',
                         query: this.searchedFilm,
@@ -33,7 +47,7 @@ var app = new Vue (
                     console.log(this.films);
                 });
 
-                axios.get(this.baseURL + 'tv', {
+                axios.get(this.baseURL + 'search/tv', {
                     params: {
                         api_key: '63f8ca53972c84a81c6dff2fbb6513fb',
                         query: this.searchedFilm,
